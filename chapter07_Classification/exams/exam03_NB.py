32f7d70641783dee9a7f41f16c0d9a0ed6467ceb
junehh98 <wnsgh1663@gmail.com>
2023-05-09 16:24:43 +0900

    230509 classification

--- chapter07_Classification/exams/exam03_NB.py
+++ chapter07_Classification/exams/exam03_NB.py
@@ -12,12 +12,22 @@ weather = pd.read_csv(r'C:\ITWILL\5_Python_ML\data\weatherAUS.csv')
 print(weather.head())
 print(weather.info())
 
+
 # 단계1. 결측치(NaN)을 가진 모든 행(row) 제거 후 data 저장    
-data = None
+weather.isnull().sum()
+
+
+data = weather.dropna(axis = 0)
+data.isnull().sum()
+
+weather.shape # (36881, 24)
+data.shape # (17378, 24)
+
 
 
 # 단계2. data에서 1,2,8,10,11,22,23 칼럼을 제외한 나머지 new_data 저장 
 cols = list(data.columns) # 전체 칼럼이름 반환  
+cols
 
 colnames = [] # 사용할 칼럼 저장 
 
@@ -26,6 +36,7 @@ for i in range(len(cols)) : # 전체 칼럼 수 만큼 반복
         colnames.append(cols[i]) 
     
 new_data = data[colnames]
+
 
 
 # 모델에서 사용할 전체 관측치 36,881개와 변수 17개  
@@ -54,23 +65,66 @@ Data columns (total 17 columns):
  
 
 # 단계3. new_data에서 변수 선택(y변수 : RainTomorrow, x변수 : 나머지 16개)
-y = None
-X = None
+y = new_data.RainTomorrow
+X = new_data.drop('RainTomorrow', axis=1)
 
 
 
 # 단계4. y변수 레이블 인코딩 
 from sklearn.preprocessing import LabelEncoder 
 
+le = LabelEncoder()
+y = le.fit_transform(y)
+
 
 
 # 단계5. 70:30 비율 train/test 데이터셋 구성
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+     X, y, test_size=0.3, random_state=100)
 
 
 # 단계6. GaussianNB 모델 생성 
-  
+gnb = GaussianNB() 
+model = gnb.fit(X_train, y_train)
+
 
 # 단계7. model 평가 : accuracy, confusion matrix, classification_report
+y_pred = model.predict(X_test)
+
+# accuracy
+y_true = y_test
+acc = accuracy_score(y_true, y_pred)
+print('accuracy =', acc) # accuracy = 0.8097429996164174
+
+
+# confision matrix
+con_mat = confusion_matrix(y_true, y_pred)
+print(con_mat)
+'''
+[[3410  643]
+ [ 349  812]]
+'''
+
+# classification_report
+report = classification_report(y_true, y_pred)
+print(report)
+'''
+              precision    recall  f1-score   support
+
+           0       0.91      0.84      0.87      4053
+           1       0.56      0.70      0.62      1161
+
+    accuracy                           0.81      5214
+   macro avg       0.73      0.77      0.75      5214
+weighted avg       0.83      0.81      0.82      5214
+'''
+
+
+
+
+
 
 
 
--- chapter07_Classification/lecture01_classifier/step03_NB_model1.py
+++ chapter07_Classification/lecture01_classifier/step03_NB_model1.py
@@ -21,6 +21,7 @@ np.set_printoptions(threshold=sys.maxsize) # 배열 전체 출력(배열 내용 
 pd.set_option('display.max_columns', 100) # 최대 칼럼수 지정 
 
 
+
 # 1. dataset load 
 X, y = load_iris(return_X_y=True)
 print(X)
@@ -30,6 +31,9 @@ from sklearn.preprocessing import StandardScaler
 
 # z-score 표준화 : 평균=0, 표준편차=1 - 정규분포를 이루고 있는 x변수 대상 스케일링 
 X = StandardScaler().fit_transform(X) # fit + transform 
+X.mean() # 0에 수렴
+X.std() # 1.0 -> 표준화된것 확인 
+
 
 
 # 2. train/test data set 구성 
@@ -40,26 +44,32 @@ print(X_train.shape) # (105, 5)
 print(X_test.shape) # (45, 5)
 
 
-# 3. NB model 생성 
+
+# 3. NB model 생성 , 학습
 gnb = GaussianNB() 
 model = gnb.fit(X_train, y_train) # y 집단변수 
+
+
 
 # 4. model 예측치 
 y_pred = model.predict(X_test)
 
 
+
 # 5. model 평가 
+
 # 1) accuracy
 y_true = y_test
 acc = accuracy_score(y_true, y_pred)
-print('accuracy =', acc) # accuracy = 0.9555555555555556
+print('accuracy =', acc) # accuracy = 0.9555555555555556의 분류정확도
+
 
 # 2) confusion matrix
 con_mat = confusion_matrix(y_true, y_pred)
 print(con_mat)
 '''
-[[18  0  0]
- [ 0 10  0]
- [ 0  2 15]]
+[[18  0  0] -> missing x
+ [ 0 10  0] -> missing x
+ [ 0  2 15]] -> 2개의 missing
 '''
 
